33620c9d851bafbc9b62dc0db270b939368dfb4d
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-27 17:16:04 -0600

    Commit before weekend

--- src/Components/Dashboard/DashPokemon/DashPokemon.jsx
+++ src/Components/Dashboard/DashPokemon/DashPokemon.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const DashPokemon = (props) => {
   return (
     <div>
-      <img src={props.data.info.sprites.front_default} alt=""/>
+      {/* <img src={props.data.info.sprites.front_default} alt=""/> */}
       {props.data.name}
     </div>
   )
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import DashPokemon from './DashPokemon/DashPokemon'
 
 const Dashboard = (props) => {
 
+  console.log(props)
+
   const [range, setRange] = useState([0, 24])
 
   const queries = [{ query: 'start', value: range[0] }, { query: 'end', value: range[1] }]
@@ -16,8 +18,12 @@ const Dashboard = (props) => {
       {pokemon && pokemon.map(element => (
         <DashPokemon key={element.info.id} data={element} />
       ))}
-      {range[0] > 0 && <button onClick={()=>setRange([range[0] - 25, range[1] - 25])}>Prev</button>}
-      {range[1] < data.max && <button onClick={()=>setRange([range[0] + 25, range[1] + 25])}>Next</button>}
+      {range[0] > 0 && <button
+        onClick={() => setRange([range[0] - 25, range[1] - 25])}
+      >Prev</button>}
+      {range[1] < data.max && <button
+        onClick={() => setRange([range[0] + 25, range[1] + 25])}>Next
+      </button>}
     </div>
   )
 }
