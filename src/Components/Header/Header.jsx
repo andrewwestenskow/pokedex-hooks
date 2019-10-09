4f355629aed97458e31d47329eb6d646525470c3
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-10-09 15:04:55 -0600

    Per page select works

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {withRouter} from 'react-router-dom'
 
 
 const Header = (props) => {
+  console.log(props.match)
+
+  const [perPage, setPerPage] = useState(50)
+
+  useEffect(() => {
+    props.history.push(`/cards/page/${props.match.params.page || 1}?perPage=${+perPage}`)
+  }, [perPage])
+
   return (
-    <div>
-      Header
+    <div className='Header'>
+      <select defaultValue="50" onChange={(e) => setPerPage(e.target.value)}>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
     </div>
   )
 }
-export default Header
+export default withRouter(Header)
--- src/Components/Redirect/Redirect.jsx
+++ src/Components/Redirect/Redirect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 const Redirect = (props) => {
-props.history.push('/cards/page/1?perPage=25')
-return <></>
+  props.history.push('/cards/page/1?perPage=50')
+  return <></>
 }
 export default Redirect
